17f5e23a51105aa7c8b8969bd97f278265c296b4
Aayush Dutt <aayushd100@gmail.com>
2024-03-25 17:33:50 +0530

    Update to use argparse and remove json config

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
+import argparse
 from concurrent.futures import ThreadPoolExecutor
 import json
 import os
 from urllib import request, error
-
-with open("config.json", "r") as f:
-    config = json.load(f)
-
-MC_VERSION = config["mc_version"]  # "1.20.4"
-LOADER = config["loader"]  # "fabric, forge, quilt"
-COLLECTION_ID = config["collection_id"]
-MOD_PATH = config["mod_path"]
-
-if not os.path.exists(MOD_PATH):
-    os.mkdir(MOD_PATH)
 
 
 class ModrinthClient:
@@ -44,12 +34,45 @@ class ModrinthClient:
 modrinth_client = ModrinthClient()
 
 
+def parse_args():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(description="Download mods from Modrinth.")
+    parser.add_argument(
+        "-c", "--collection", required=True, help="ID of the collection to download."
+    )
+    parser.add_argument("-v", "--version", required=True, help="Minecraft version.")
+    parser.add_argument(
+        "-l",
+        "--loader",
+        required=True,
+        help='Loader to use ("fabric", "forge", "quilt" etc.).',
+    )
+    parser.add_argument(
+        "-d", "--directory", default="./mods", help="Directory to download mods to."
+    )
+    parser.add_argument(
+        "-u",
+        "--update",
+        default=False,
+        action="store_true",
+        help="Update existing mods.",
+    )
+    return parser.parse_args()
+
+
+args = parse_args()
+
+if args.directory:
+    if not os.path.exists(args.directory):
+        os.mkdir(args.directory)
+
+
 def get_existing_mods() -> list[dict]:
-    file_names = os.listdir(MOD_PATH)
-    return [{
-        "id": file_name.split(".")[-2],
-        "filename": file_name
-    } for file_name in file_names]
+    file_names = os.listdir(args.directory)
+    return [
+        {"id": file_name.split(".")[-2], "filename": file_name}
+        for file_name in file_names
+    ]
 
 
 def get_latest_version(mod_id):
@@ -59,65 +82,75 @@ def get_latest_version(mod_id):
         return None
 
     mod_version_to_download = next(
-        (mod_version for mod_version in mod_versions_data
-         if MC_VERSION in mod_version["game_versions"]
-         and LOADER in mod_version["loaders"]), None)
+        (
+            mod_version
+            for mod_version in mod_versions_data
+            if args.version in mod_version["game_versions"]
+            and args.loader in mod_version["loaders"]
+        ),
+        None,
+    )
     return mod_version_to_download
 
 
-def download_mod(mod_id, update=False, existing_mods=[]):
-    existing_mod = next((mod for mod in existing_mods if mod["id"] == mod_id),
-                        None)
-    if not update and existing_mod:
-        print(f"{mod_id} already exists, skipping...")
-        return
+def download_mod(mod_id, existing_mods=[]):
+    try:
+        existing_mod = next((mod for mod in existing_mods if mod["id"] == mod_id), None)
 
-    latest_mod = get_latest_version(mod_id)
-    if not latest_mod:
-        print(
-            f"No version found for {mod_id} with MC_VERSION={MC_VERSION} and LOADER={LOADER}"
+        if not args.update and existing_mod:
+            print(f"{mod_id} already exists, skipping...")
+            return
+
+        latest_mod = get_latest_version(mod_id)
+        if not latest_mod:
+            print(
+                f"No version found for {mod_id} with MC_VERSION={args.version} and LOADER={args.loader}"
+            )
+            return
+
+        file_to_download: dict | None = next(
+            (file for file in latest_mod["files"] if file["primary"] == True), None
         )
-        return
+        if not file_to_download:
+            print(f"Couldn't find a file to download for {mod_id}")
+            return
+        filename: str = file_to_download["filename"]
+        filename_parts = filename.split(".")
+        filename_parts.insert(-1, mod_id)
+        filename_with_id = ".".join(filename_parts)
 
-    file_to_download: dict | None = next(
-        (file for file in latest_mod["files"] if file['primary'] == True),
-        None)
-    if not file_to_download:
-        print(f"Couldn't find a file to download for {mod_id}")
-        return
-    filename: str = file_to_download["filename"]
-    filename_parts = filename.split(".")
-    filename_parts.insert(-1, mod_id)
-    filename_with_id = ".".join(filename_parts)
+        if existing_mod and existing_mod["filename"] == filename_with_id:
+            print(f"{filename_with_id} latest version already exists.")
+            return
 
-    if existing_mod and existing_mod["filename"] == filename_with_id:
-        print(f"{filename_with_id} latest version already exists.")
-        return
+        print(
+            "UPDATING: " if existing_mod else "DOWNLOADING: ",
+            file_to_download["filename"],
+            latest_mod["loaders"],
+            latest_mod["game_versions"],
+        )
+        modrinth_client.download_file(
+            file_to_download["url"], f"{args.directory}/{filename_with_id}"
+        )
 
-    print("Updating: " if existing_mod else "Downloading: ",
-          file_to_download['filename'], latest_mod['loaders'],
-          latest_mod['game_versions'])
-    modrinth_client.download_file(file_to_download["url"],
-                                  f"{MOD_PATH}/{filename_with_id}")
-
-    if existing_mod:
-        print(f"Removing previous version: {existing_mod['filename']}")
-        os.remove(f"{MOD_PATH}/{existing_mod['filename']}")
+        if existing_mod:
+            print(f"REMOVING previous version:  {existing_mod['filename']}")
+            os.remove(f"{args.directory}/{existing_mod['filename']}")
+    except Exception as e:
+        print(f"Failed to download {mod_id}: {e}")
 
 
 def main():
-    collection_details = modrinth_client.get_collection(COLLECTION_ID)
+    collection_details = modrinth_client.get_collection(args.collection)
     if not collection_details:
-        print(f"Collection id={COLLECTION_ID} not found")
+        print(f"Collection id={args.collection} not found")
         return
     mods: str = collection_details["projects"]
     print("Mods in collection: ", mods)
     existing_mods = get_existing_mods()
-    should_update = "--update" in os.sys.argv or "--upgrade" in os.sys.argv
 
     with ThreadPoolExecutor(max_workers=5) as executor:
-        executor.map(download_mod, mods, [should_update] * len(mods),
-                     [existing_mods] * len(mods))
+        executor.map(download_mod, mods, [existing_mods] * len(mods))
 
 
 if __name__ == "__main__":
